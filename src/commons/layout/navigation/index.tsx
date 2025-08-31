db1dda7655f5f409d45dc1ce584109251b45fecd
jjontteok <nayeong0121@gmail.com>
2025-08-31 18:15:07 +0900

    이력서 버튼 없앰

--- src/commons/layout/navigation/index.tsx
+++ src/commons/layout/navigation/index.tsx
@@ -14,12 +14,12 @@ export default function LayoutNavigation() {
     router.push("/portfolio");
   };
 
-  const onClickResumeDown = () => {
-    const resumeLink = document.createElement("a"); //동적으로 <a>태그 생성
-    resumeLink.href = "/files/kny_resume.pdf"; //다운 받을 파일
-    resumeLink.download = "김나영_이력서.pdf"; //다운 받을 때 지정될 파일 이름
-    resumeLink.click(); //a태그를 클릭한 걸로 지정
-  };
+  // const onClickResumeDown = () => {
+  //   const resumeLink = document.createElement("a"); //동적으로 <a>태그 생성
+  //   resumeLink.href = "/files/kny_resume.pdf"; //다운 받을 파일
+  //   resumeLink.download = "김나영_이력서.pdf"; //다운 받을 때 지정될 파일 이름
+  //   resumeLink.click(); //a태그를 클릭한 걸로 지정
+  // };
 
   return (
     <div className={styles.navigation}>
@@ -29,9 +29,9 @@ export default function LayoutNavigation() {
       <button onClick={onClickPortfolio} className={styles.menu}>
         Portfolio
       </button>
-      <button onClick={onClickResumeDown} className={styles.resume}>
+      {/* <button onClick={onClickResumeDown} className={styles.resume}>
         이력서 다운로드
-      </button>
+      </button> */}
     </div>
   );
 }
